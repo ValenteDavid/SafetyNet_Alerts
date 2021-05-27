bd2ada3e41673fff51286d6f70d0458ebff40cbd
David Valente <dvalente77@gmail.com>
2021-05-27 19:49:08 +0200

    add maven profil and validator config

--- src/main/java/com/safetynet/safetynetalerts/SafetyNetAlertsApplication.java
+++ src/main/java/com/safetynet/safetynetalerts/SafetyNetAlertsApplication.java
@@ -2,27 +2,14 @@ package com.safetynet.safetynetalerts;
 
 import java.io.File;
 import java.nio.file.Files;
-import java.util.Iterator;
-import java.util.List;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.safetynet.safetynetalerts.model.FireStation;
-import com.safetynet.safetynetalerts.model.MedicalRecord;
-import com.safetynet.safetynetalerts.model.Person;
 import com.safetynet.safetynetalerts.repository.DataFile;
-import com.safetynet.safetynetalerts.repository.FireStationImpl;
-import com.safetynet.safetynetalerts.repository.MedicalRecordImpl;
-import com.safetynet.safetynetalerts.repository.PersonDaoImpl;
 
 @SpringBootApplication
 public class SafetyNetAlertsApplication {
@@ -36,7 +23,7 @@ public class SafetyNetAlertsApplication {
 	CommandLineRunner runner() {
 		return args -> {
 			File fileUse = new File("src/main/resources/data.json");
-			File fileLoad = new File("src/main/resources/data-original.json");
+			File fileLoad = new File("src/main/resources/data-test.json");
 			if (fileUse.exists()) {
 				fileUse.delete();
 			}
